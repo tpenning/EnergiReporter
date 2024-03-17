19f1743766eefc5d7746edf6ada006dded4ea69b
Thijs Penning <t.j.penning@student.tudelft.nl>
2024-03-17 15:59:48 +0100

    Add the single data power intervals boxplot

--- pages/1_📈_Single_Measurement.py
+++ pages/1_📈_Single_Measurement.py
@@ -1,3 +1,4 @@
+import altair as alt
 import pandas as pd
 import streamlit as st
 
@@ -27,6 +28,19 @@ def generate_et_charts(tdf):
     tab_bar.bar_chart(energy_time_df, x="Time (s)", y="Energy (J)", use_container_width=True)
 
 
+def generate_power_boxplot_chart(tdf):
+    # Calculate the power values at all time intervals
+    power_values = []
+    for i in range(len(df.values) - 1):
+        power_values.append(df.values[i][1] / (df.values[i+1][0] - df.values[i][0]))
+    pdf = pd.DataFrame(data={"Power (W)": power_values})
+
+    # Create the boxplot chart
+    st.subheader("Energy data over time:")
+    chart = alt.Chart(pdf).mark_boxplot(extent="min-max").encode(y="Power (W)").interactive()
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+
+
 # File uploader
 uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
@@ -34,5 +48,6 @@ if uploaded_file is not None:
     # Read the CSV file
     df = pd.read_csv(uploaded_file)
 
-    # Generate the energy over time chart tabs
+    # Generate the energy over time chart tabs and the power intervals boxplot
     generate_et_charts(df)
+    generate_power_boxplot_chart(df)
