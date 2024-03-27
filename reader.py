7dac998620928e5789afa75582e986aadddaa5e9
Milan <77207511+mdek2053@users.noreply.github.com>
2024-03-27 17:06:45 +0100

    Add EnergiBridge data file extraction

--- reader.py
+++ reader.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Easy to use/rename variables for the columns used
-POWER = "Power (W)"
 TIME = "Time (s)"
+POWER = "Power (W)"
 
 
 # TODO: Test with scripts that have different time intervals (especially power extraction)
 def read_uploaded_files(uploaded_files):
     # Initialize the values to (intermediately) store the data
-    total_energy = 0
+    total_energies = []
     pdfs = []
     names = []
 
@@ -18,25 +18,71 @@ def read_uploaded_files(uploaded_files):
         df = pd.read_csv(uploaded_file)
         names.append(uploaded_file.name[:-4])
 
-        # Add the total energy of this data file
-        total_energy += df["ENERGY"].sum()
+        # Extract the time-power DataFrame and the total energy used
+        power_tdf, total_energy = extract_df(df)
 
-        # Create the power DataFrame, index it over time and add the power column to the list
-        pdf = pd.DataFrame(data={"Time (s)": df["TIME"],
-                                 "POWER": [0] + [df.values[i][1] / (df.values[i + 1][0] - df.values[i][0])
-                                                 for i in range(len(df.values) - 1)]})
-        pdf.set_index("Time (s)", inplace=True)
-        pdfs.append(pdf["POWER"])
+        # Add the total energy to the list
+        total_energies.append(total_energy)
+
+        # Index the pdf over time and add the power column to the pdfs list
+        power_tdf.set_index(TIME, inplace=True)
+        pdfs.append(power_tdf[POWER])
 
     # Concatenate the DataFrames along the POWER columns and calculate the mean data across the DataFrames
     power_df = pd.concat(pdfs, axis=1, keys=names)
-    mean_df = power_df.mean(axis=1).to_frame().rename(columns={0: "Power (W)"})
-
-    # Average the total energy over all files
-    total_energy = round(total_energy / len(uploaded_files), 2)
+    mean_df = power_df.mean(axis=1).to_frame().rename(columns={0: POWER})
 
     # Create a pdfs copy without the time indexing for the data statistics
     stat_pdfs = [df.reset_index(drop=True) for df in pdfs]
 
     # Return the retrieved data formats and information
-    return power_df, mean_df, total_energy, names, stat_pdfs
+    return power_df, mean_df, total_energies, names, stat_pdfs
+
+
+def extract_df(df):
+    # The variables used to retrieve the energy and power
+    total_time = 0
+    time = []
+    total_energy = 0
+    power = []
+
+    # Extract the power and energy corresponding to the data column available
+    if "CPU_POWER (Watts)" in df.columns or "SYSTEM_POWER (Watts)" in df.columns:
+        # Get the column key and the power immediately
+        key = "CPU_POWER (Watts)" if "CPU_POWER (Watts)" in df.columns else "SYSTEM_POWER (Watts)"
+        power = df[key].tolist()
+
+        # Calculate the time and total energy
+        for index, row in df.iloc[2:].iterrows():
+            # Update the total time and add the 0.1s rounded time to the list
+            delta = (row["Time"] - df.at[index - 1, "Time"]) / 1000
+            total_time += delta
+            time.append(round(total_time, 1))
+
+            # Calculate the energy used over the last delta and add it to the total
+            total_energy += row[key] * delta
+    elif "CPU_ENERGY (J)" in df.columns or "PACKAGE_ENERGY (J)" in df.columns:
+        # Get the column key
+        key = "CPU_ENERGY (J)" if "CPU_ENERGY (J)" in df.columns else "PACKAGE_ENERGY (J)"
+
+        # Calculate the time, energy, and power
+        for index, row in df.iloc[2:].iterrows():
+            # Update the total time and add the 0.1s rounded time to the list
+            delta = (row["Time"] - df.at[index - 1, "Time"]) / 1000
+            total_time += delta
+            time.append(round(total_time, 1))
+
+            # Calculate the energy used in the delta from the difference and add it to the total
+            energy = row[key] - df.at[index - 1, key]
+            total_energy += energy
+
+            # Calculate the power consumed used over the last delta and add it to the list
+            power.append(energy / delta)
+    else:
+        raise ValueError("None of the specified energy data columns are present in the CSV file")
+
+    # Create a DataFrame for the time and power
+    power_tdf = pd.DataFrame(data={TIME: time, POWER: power})
+
+    # Return the time-power DataFrame and the total energy consumption
+    return power_tdf, total_energy
